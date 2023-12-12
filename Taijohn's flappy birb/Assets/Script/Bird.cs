69bd62c05bcb7bf70d3a91154396b0030e3c13ed
Taijohn Jones <145067431+TaijohnnBoi@users.noreply.github.com>
2023-12-12 13:52:03 -0800

    Added animations
    Idle, Flap. and Die has been added

---
+++ Taijohn's flappy birb/Assets/Script/Bird.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour { 
+
+    public float upforce = 200f;
+
+    private bool IsDead = false;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsDead == false)
+        {
+            if (Input.GetMouseButtonDown (0))
+            {
+
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2 (0, upforce));
+                anim.SetTrigger("Flap");
+
+            }
+        }
+    }
+
+    void OnCollisionEnter2D ()
+    {
+        IsDead = true;
+        anim.SetTrigger("Die");
+    }
+}
